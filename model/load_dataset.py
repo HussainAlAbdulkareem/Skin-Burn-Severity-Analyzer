582548133559abe42cf43e657f67c35be9dcde73
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2025-07-26 19:55:49 -0600

    Resize Images

--- model/load_dataset.py
+++ model/load_dataset.py
@@ -1,15 +1,22 @@
+from torch import tensor
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
 
 transform = transforms.Compose([
-    transforms.ToTensor()
+    transforms.Resize([224,224]),
+    transforms.ToTensor(),
+    
 ])
 
-dataset = ImageFolder(root='Skin-Burn-Severity-Analyzer\dataset', transform=transform)
+dataset = ImageFolder(root='Skin-Burn-Severity-Analyzer\\dataset', transform=transform)
 
 
 
-print(f"Total samples: {len(dataset)}")
-print(f"Classes: {dataset.classes}")
-print(f"Class to index map: {dataset.class_to_idx}")
+# print(f"Total samples: {len(dataset)}")
+# print(f"Classes: {dataset.classes}")
+# print(f"Class to index map: {dataset.class_to_idx}")
+t = dataset[0][0]
+
+print(t.shape)
+
