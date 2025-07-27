3829a77f144a9ff961ef42d50be569195a547703
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2025-07-27 00:06:06 -0600

    Define Device

--- model/train_model.py
+++ model/train_model.py
@@ -1,9 +1,14 @@
 import torchvision.models as models
 import torch.nn as nn
+import torch
 
 model = models.resnet18(pretrained=True)
 
 
 model.fc = nn.Linear(512, 3)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
 
 print(model.fc)
