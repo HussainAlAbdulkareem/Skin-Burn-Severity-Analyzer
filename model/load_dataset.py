76a4b4e01341769f76a46bded629573eba79c971
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2025-07-26 21:18:01 -0600

    Split and Shuffle dataset

--- model/load_dataset.py
+++ model/load_dataset.py
@@ -1,10 +1,15 @@
 import matplotlib
 import matplotlib.pyplot as plt
 from torch import tensor
+import torch
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 import numpy as np
+from torch.utils.data.dataset import random_split
+from torch.utils.data import DataLoader
 
+
+torch.manual_seed(42)
 
 transform = transforms.Compose([
     transforms.Resize([224,224]),
@@ -14,12 +19,26 @@ transform = transforms.Compose([
 
 dataset = ImageFolder(root='Skin-Burn-Severity-Analyzer\\dataset', transform=transform)
 
-t = dataset[0][0]
 
-t_numpy = t.numpy()
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+
+train_set, val_set = random_split(dataset, [train_size, val_size], seed=42)
 
 
-transposed_arr = np.transpose(t_numpy, axes=(1, 2, 0))
+train_dataloader = DataLoader(train_set, batch_size=32, shuffle=True)
+val_dataloader = DataLoader(val_set, batch_size=32, shuffle=True)
 
-plt.imshow(transposed_arr)
-plt.show()
+
+print(train_dataloader)
+print(val_dataloader)
+
+# t = dataset[0][0]
+
+# t_numpy = t.numpy()
+
+
+# transposed_arr = np.transpose(t_numpy, axes=(1, 2, 0))
+
+# plt.imshow(transposed_arr)
+# plt.show()
