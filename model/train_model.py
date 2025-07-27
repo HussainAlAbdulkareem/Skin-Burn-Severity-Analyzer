e74bfacb593b926936a47641b3d796b02abfed69
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2025-07-27 13:13:03 -0600

    Start Validation Testing

--- model/train_model.py
+++ model/train_model.py
@@ -38,3 +38,30 @@ for epoch in range(num_epochs):
         
         
         optimizer.step()
+        
+model.eval()
+val_loss = 0
+correct = 0
+total = 0
+
+
+with torch.no_grad():
+    for images, labels in val_dataloader:
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        outputs = model(images)        
+        loss = criterion(outputs, labels)
+        val_loss += loss.item()
+        
+        preds = torch.argmax(outputs, dim=1)
+        
+        correct += (preds == labels).sum().item()
+        total += labels.size(0)
+        
+avg_val_loss = val_loss / len(val_dataloader)
+val_acc = correct / total 
+
+
+print(avg_val_loss)
+print(val_acc)
